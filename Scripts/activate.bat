30f8ec97e24fb053e9f5b22bc40679aa14ed3daf
Fenix3123 <63925178+Fenix3123@users.noreply.github.com>
2023-02-27 18:31:26 -0600

    Some changes

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Fenix Xia\Documents\GitHub\proj0
+set VIRTUAL_ENV=C:\Users\Fenix Xia\Documents\GitHub\proj0_2\proj0_2
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(proj0) %PROMPT%
+set PROMPT=(proj0_2) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(proj0) 
+set VIRTUAL_ENV_PROMPT=(proj0_2) 
 
 :END
 if defined _OLD_CODEPAGE (
